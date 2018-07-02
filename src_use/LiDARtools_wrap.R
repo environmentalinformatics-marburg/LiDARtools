cdb33588d70bf26e67ddcccb37050a630348ca67
root <root@DESKTOP-OMO6G78.localdomain>
2018-07-02 13:04:06 +0200

    minor changes

--- src_use/LiDARtools_wrap.R
+++ src_use/LiDARtools_wrap.R
@@ -15,7 +15,7 @@ library(LiDARtools)
 
 #Sources:
 setwd("D:/Uni/Projekte/Kili/src/")
-sub <- "jun18_50m/"
+sub <- "jul18_50m/"
 inpath <- "../data/" # only original files
 dat_path <- paste0("../data/", sub)
 if (file.exists(dat_path)==F){
@@ -52,8 +52,9 @@ gap_frac_path <- "gap_structure"
 dat_SR_path <- "dat_SR"
 traits_path <- "traits"
 SR_residuals_path <- "SR_residuals"
+beta_residuals_path <- "beta_residuals"
 beta_anm_plnt_path <- "beta_anm_plnt"
-lst_vars_path_field <- c(dat_SR_path, SR_residuals_path, beta_anm_plnt_path, traits_path)
+lst_vars_path_field <- c(dat_SR_path, SR_residuals_path, beta_anm_plnt_path, beta_residuals_path, traits_path)
 lst_vars_path_ldr <- c(db_str_path, point_str_path, gap_frac_path)
 # lst_vars_path <- c(dat_SR_path, SR_residuals_path, beta_anm_plnt_path, traits_path,
 #                    db_str_path, point_str_path, gap_frac_path)
